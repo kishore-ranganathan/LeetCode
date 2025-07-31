71f685a671e60ec63174cd180f0cf95d1a8adcf9
kishore-ranganathan <kishore.r2125@gmail.com>
2025-07-31 13:57:52 +0530

    Time: 20 ms (5.15%), Space: 45.1 MB (35.43%) - LeetHub

---
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] getFinalState(int[] nums, int k, int multiplier) {
+       
+        while(k-- !=0){
+            int max = nums[0];
+             int ind = 0;
+            for(int i=1;i<nums.length;i++){
+                if(max > nums[i]) {
+                    max = nums[i];
+                    ind = i;
+                }
+                
+            }
+            System.out.print(max+" ");
+            System.out.println(max*multiplier+" ");
+
+            nums[ind] = max*multiplier;
+        }
+        return nums;
+    }
+}
